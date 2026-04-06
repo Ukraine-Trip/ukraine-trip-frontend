f045ef97c9f991d1a6d7d86f669f665ed764ab03
Oleh Tsap <olehtsap07@gmail.com>
2026-04-06 17:52:27 +0300

    add interface for Pixabay

---
+++ src/types/pixabay.ts
@@ -0,0 +1,25 @@
+export interface PixabayImage {
+  id: number;
+  pageURL: string;
+  type: string;
+  tags: string;
+  previewURL: string;
+  webformatURL: string;
+  largeImageURL: string;
+  imageWidth: number;
+  imageHeight: number;
+  views: number;
+  downloads: number;
+  collections: number;
+  likes: number;
+  comments: number;
+  user_id: number;
+  user: string;
+  userImageURL: string;
+}
+
+export interface PixabayResponse {
+  total: number;
+  totalHits: number;
+  hits: PixabayImage[];
+}

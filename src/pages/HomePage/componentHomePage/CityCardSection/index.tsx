24345ca7799e981b00782563456cf2b746a4698b
wert1ty32 <wert1ty32@gmail.com>
2026-04-22 21:24:07 +0300

    increasedfunctionality

--- src/pages/HomePage/componentHomePage/CityCardSection/index.tsx
+++ src/pages/HomePage/componentHomePage/CityCardSection/index.tsx
@@ -1,14 +1,10 @@
-import React from 'react';
-import {
-  Box,
-  Container,
-  Button,
-  Card,
-  CardMedia,
-  CardActionArea,
-} from '@mui/material';
+import React, { useState, useMemo } from 'react';
+import { Box, Container, Card, CardMedia, CardActionArea } from '@mui/material';
 import type { CityCardData } from '../ContentData/types';
 import { CardsSection, CityCard, CityNameOverlay } from '../../style.tsx';
+import { PrimaryButton } from '../../../../style/common.tsx';
+import ExpandLessIcon from '@mui/icons-material/ExpandLess';
+import ArrowUpwardIcon from '@mui/icons-material/ArrowUpward';
 
 interface CityCardsSectionProps {
   cities: CityCardData[];
@@ -17,6 +13,29 @@ interface CityCardsSectionProps {
 export const CityCardsSection: React.FC<CityCardsSectionProps> = ({
   cities,
 }) => {
+  const [isExpanded, setIsExpanded] = useState(false);
+
+  const uniqueCities = useMemo(() => {
+    const seenIds = new Set();
+    return cities.filter((city) => {
+      if (seenIds.has(city.id)) {
+        return false;
+      }
+      seenIds.add(city.id);
+      return true;
+    });
+  }, [cities]);
+
+  const visibleCities = isExpanded ? uniqueCities : uniqueCities.slice(0, 3);
+
+  const handleShowMore = () => {
+    setIsExpanded(true);
+  };
+
+  const handleCollapse = () => {
+    setIsExpanded(false);
+  };
+
   return (
     <CardsSection>
       <Container maxWidth="lg">
@@ -32,7 +51,7 @@ export const CityCardsSection: React.FC<CityCardsSectionProps> = ({
             mb: 3,
           }}
         >
-          {cities.map((city) => (
+          {visibleCities.map((city) => (
             <CityCard as={Card} key={city.id}>
               <CardActionArea href={city.linkUrl}>
                 <CardMedia
@@ -49,20 +68,20 @@ export const CityCardsSection: React.FC<CityCardsSectionProps> = ({
           ))}
         </Box>
 
-        <Box sx={{ display: 'flex', justifyContent: 'center', mt: 4 }}>
-          <Button
-            variant="contained"
-            sx={{
-              backgroundColor: '#000',
-              color: '#fff',
-              px: 6,
-              py: 1.5,
-              '&:hover': { backgroundColor: '#333' },
-            }}
-          >
-            VIEW MORE
-          </Button>
-        </Box>
+        {!isExpanded && uniqueCities.length > 3 && (
+          <Box sx={{ display: 'flex', justifyContent: 'center', mt: 4 }}>
+            <PrimaryButton onClick={handleShowMore}>View more</PrimaryButton>
+          </Box>
+        )}
+
+        {isExpanded && (
+          <Box sx={{ display: 'flex', justifyContent: 'center', mt: 4 }}>
+            <PrimaryButton
+              onClick={handleCollapse}
+              startIcon={<ExpandLessIcon />}
+            </PrimaryButton>
+          </Box>
+        )}
       </Container>
     </CardsSection>
   );
